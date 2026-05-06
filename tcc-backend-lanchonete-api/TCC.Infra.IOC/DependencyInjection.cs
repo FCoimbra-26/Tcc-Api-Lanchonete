54a8a43ba1e1c5e7b99a390ab6b62628a8bcebae
Felipe Coimbra <felipecoimbra@outlook.com.br>
2026-05-05 23:10:00 -0300

    Iniciando DBContext e fix versão de dependencia

--- tcc-backend-lanchonete-api/TCC.Infra.IOC/DependencyInjection.cs
+++ tcc-backend-lanchonete-api/TCC.Infra.IOC/DependencyInjection.cs
@@ -1,15 +1,25 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Data.Common;
 using TCC.Application.Service.Interfaces;
 using TCC.Application.Service.Services;
+using TCC.Infra.Data.Context;
 using TCC.Infra.Security.Interfaces;
 using TCC.Infra.Security.Token;
+
 
 namespace TCC.Infra.IOC
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("TCC"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            
+
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<ILoginService, LoginService>();
             return services;
--- tcc-backend-lanchonete-api/tcc-backend-lanchonete-api/Program.cs
+++ tcc-backend-lanchonete-api/tcc-backend-lanchonete-api/Program.cs
@@ -8,8 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddInfrastructure(); // Adicione aqui os argumentos necessários, se houver.
-
+builder.Services.AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
